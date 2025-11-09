002f2f1a3886066cd2786684f713d619106c88bd
Mynkskhr <31133119+Mynkskhr@users.noreply.github.com>
2025-11-09 13:59:58 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,11 +3,11 @@
 # -- Project information
 
 project = 'Thinkwerke'
-copyright = '2025, Owned by Thinkwerke'
+copyright = '2025, By Thinkwerke'
 author = 'Mayank Sekhar'
 
 release = '0.1'
-version = '0.1.0'
+version = '1.0'
 
 # -- General configuration
 
